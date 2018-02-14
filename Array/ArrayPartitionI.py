e7024c1148191426461347fc17b54a87b348c7a9
Claudia Flores Saviaga <saviaga@hotmail.com>
2018-02-14 10:07:04 -0600

    Array

--- Array/ArrayPartitionI.py
+++ Array/ArrayPartitionI.py
@@ -1,14 +1,15 @@
 """
-      Given an array of 2n integers, your task is to group these integers into n pairs of integer,
-      say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
-      Input: [1,4,3,2]
-      Output: 4
-      Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+https://leetcode.com/problems/array-partition-i/description/
+Given an array of 2n integers, your task is to group these integers into n pairs of integer,
+say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+Input: [1,4,3,2]
+Output: 4
+Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
 
       """
 
-class Solution(object):
-    def arrayPairSum(self, nums):
+
+def arrayPairSum(nums):
         """
         :type nums: List[int]
         :rtype: int
@@ -21,6 +22,5 @@ class Solution(object):
 
 if __name__=="__main__":
     array=[1,4,3,2]
-    PairSum = Solution()
-    result=PairSum.arrayPairSum(array)
+    result=arrayPairSum(array)
     print(result)
